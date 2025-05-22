6c2ac721a9670dee136bbaab59f5197ebf94c48e
yaWhitekali <dawju9@gmail.com>
2025-05-22 22:02:28 +0200

    ew

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import { Scratchcard } from './components/scratchcard'
+import { ScratchCard } from './components/scratchcard'
 
 export default function App() {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Scratchcard width={undefined} height={undefined} image={undefined} finishPercent={undefined} onComplete={undefined} children={undefined} />
+    <ScratchCard width={300} height={300} image={"placeholder.jpg"} finishPercent={70} onComplete={() => {}} children={<div>Scratch me!</div>} />
   </React.StrictMode>
 )
