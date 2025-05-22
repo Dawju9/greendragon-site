3e8c51326b7b6ce4b79dab3dc9d66bf047cd4468
yaWhitekali <dawju9@gmail.com>
2025-05-22 22:10:11 +0200

    bb

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ScratchCard } from './components/scratchcard';
 
 export default function App() {
   return (
-    <ScratchCard width={300} height={300} image={"https://example.com/image.jpg"} finishPercent={70} onComplete={() => {}} children={<div>Scratch me!</div>} />
+    <ScratchCard width={300} height={300} image={"../src/zdrapa.jpg"} finishPercent={70} onComplete={() => {}} children={<div>Scratch me!</div>} />
   );
 }
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './App.css'
-import { ScratchCard } from './components/scratchcard'
 
-export default function App() {
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ScratchCard width={300} height={300} image={"placeholder.jpg"} finishPercent={70} onComplete={() => {}} children={<div>Scratch me!</div>} />
-  </React.StrictMode>
-)
